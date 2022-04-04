ea8d2f0cdeb47463d6149ea489c16ad1990c04f2
Jonathan Gamble <jdgamble555@gmail.com>
2022-04-03 21:11:32 -0500

    endpoint

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
-import { handler } from 'api/handler';
+import { handler } from 'handler';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
