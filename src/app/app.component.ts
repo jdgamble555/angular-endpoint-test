d0b2b4c1f9697deb8afe3b997c130f9d49b9dbdb
Jonathan Gamble <jdgamble555@gmail.com>
2022-04-05 04:52:14 -0500

    add state transfer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, Inject, Optional, PLATFORM_ID } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { REQUEST } from '@nguniversal/express-engine/tokens';
+import { makeStateKey, TransferState } from '@angular/platform-browser';
 
-declare const Zone: any;
 
 @Component({
   selector: 'app-root',
@@ -23,21 +23,31 @@ export class AppComponent {
     @Inject(PLATFORM_ID) platformId: Object,
     @Optional() @Inject(REQUEST) private request: any,
     @Inject(DOCUMENT) private document: Document,
+    private transferState: TransferState,
     private http: HttpClient
   ) {
     this.isServer = isPlatformServer(platformId);
 
-    // get base url
     if (this.isServer) {
-      this.baseURL = this.request.headers.referer;
-    } else {
-      this.baseURL = this.document.location.origin + '/';
-    }
 
-    // grab data
-    this.getData().then((data) => this.data = data.r);
+      // get data on server, save state
+      // get base url from request obj
+      this.baseURL = this.request.headers.referer;
+      this.getData();
+    } else {
+
+      // retrieve state on browser
+      // get base url from location obj
+      if (this.hasState('posts')) {
+        this.data = this.getState('posts');
+      } else {
+        this.baseURL = this.document.location.origin + '/';
+        this.getData();
+      }
+    }
   }
 
+  // get data from REST API
   async getData(): Promise<any> {
     return await firstValueFrom(
       this.http.get(this.baseURL + 'api/me', {
@@ -46,6 +56,27 @@ export class AppComponent {
         },
         responseType: 'json'
       })
-    );
+    ).then((data: any) => this.data = data.r);
   };
+
+  //
+  // transfer state functions
+  //
+
+  saveState<T>(key: string, data: any): void {
+    this.transferState.set<T>(makeStateKey(key), data);
+  }
+
+  getState<T>(key: string, defaultValue: any = []): T {
+    const state = this.transferState.get<T>(
+      makeStateKey(key),
+      defaultValue
+    );
+    this.transferState.remove(makeStateKey(key));
+    return state;
+  }
+
+  hasState<T>(key: string) {
+    return this.transferState.hasKey<T>(makeStateKey(key));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserTransferStateModule,
     AppRoutingModule,
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
