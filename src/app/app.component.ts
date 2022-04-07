c2510ac29975d9d74edf552eea699fa5952b9bbf
Jonathan Gamble <jdgamble555@gmail.com>
2022-04-07 00:02:06 -0500

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,42 @@ export class AppComponent {
 
   constructor(private rest: RestService) {
     this.data = this.rest.data;
+
+    import { Component } from '@angular/core';
+
+import { RestService } from './rest.service';
+import { StateService } from './state.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title = 'angular-test';
+  data: string;
+
+  constructor(
+    private rest: RestService,
+    private state: StateService
+  ) {
+    this.data = this.rest.data;
+
+    if (typeof window === 'undefined') {
+      if (process.env['firebase']) {
+        const fb = JSON.parse(process.env['firebase']);
+        this.state.saveState('fb', fb);
+      }
+    } else {
+      if (this.state.hasState('fb')) {
+        const fb = this.state.getState('fb');
+        console.log(fb);
+        console.log(fb.apiKey);
+      }
+    }
+  }
+}
   }
 }
