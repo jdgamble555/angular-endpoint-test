c0de80f36d705a8653bd0bb8b2dd678f6097f607
Jonathan Gamble <jdgamble555@gmail.com>
2022-04-07 00:10:12 -0500

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RestService } from './rest.service';
-import { StateService } from './state.service';
 
 @Component({
   selector: 'app-root',
@@ -12,23 +11,7 @@ export class AppComponent {
   title = 'angular-test';
   data: string;
 
-  constructor(
-    private rest: RestService,
-    private state: StateService
-  ) {
+  constructor(private rest: RestService) {
     this.data = this.rest.data;
-
-    if (typeof window === 'undefined') {
-      if (process.env['firebase']) {
-        const fb = JSON.parse(process.env['firebase']);
-        this.state.saveState('fb', fb);
-      }
-    } else {
-      if (this.state.hasState('fb')) {
-        const fb = this.state.getState('fb');
-        console.log(fb);
-        console.log((fb as any).apiKey);
-      }
-    }
   }
 }
